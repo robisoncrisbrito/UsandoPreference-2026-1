70b1316a7e985161d071ac4d96da5bab44dfaeec
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2026-04-02 19:10:55 -0300

    com o código kotlin, mas sem persistencia

--- app/src/main/java/br/edu/utfpr/usandopreference/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandopreference/MainActivity.kt
@@ -2,12 +2,17 @@ package br.edu.utfpr.usandopreference
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var image: ImageView
+    var ligado = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,7 +22,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        image = findViewById(R.id.image)
     }
 
-    fun btOnOffOnClick(view: View) {}
+    fun btOnOffOnClick(view: View) {
+        when (ligado) {
+            true -> {
+                image.setImageResource( android.R.drawable.star_big_off)
+                ligado = false
+            }
+            false -> {
+                image.setImageResource( android.R.drawable.star_big_on)
+                ligado = true
+            }
+        }
+
+    }
 }
